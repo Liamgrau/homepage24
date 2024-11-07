3d8485ba8adf4f47127c6c5f2e1761152e3f37fb
Liamgrau <145498396+Liamgrau@users.noreply.github.com>
2024-11-07 08:15:37 -0600

    11-7

--- myScripts.js
+++ myScripts.js
@@ -13,4 +13,37 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
    }
-   
+   if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+ }
+  
+ function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+ }
+ 
+ //POST-IT NOTE SHOW AND HIDE
+ var show = false;
+ function showNote(){
+    if (show == false){
+        document.getElementById("note").style.opacity = 1;
+        show = true;
+    }
+    else if (show == true){
+        document.getElementById("note").style.opacity = 0;
+        show = false;
+    }
+ }
+//CALCULATOR (JAVASCRIPT)
+var showCalc = false;
+function showCalculator(){
+   if (showCalc == false){
+       document.getElementById("calc").style.opacity = 1;
+       showCalc = true;
+   }
+   else if (showCalc == true){
+       document.getElementById("calc").style.opacity = 0;
+       showCalc = false;
+   }
+}
